7313992a8468a9936c126f9c1b11559be3b531e7
Piotr Zduński <zdunski.piotrek@gmail.com>
2020-09-04 19:21:33 +0200

    Updates

---
+++ precache-manifest.51528d77fa89de4738175b421ee06aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629ee8d8a5e5f9d63c873ac2bdba138a",
+    "url": "/showmethepicture/index.html"
+  },
+  {
+    "revision": "f04818f02d04c1dd67b6",
+    "url": "/showmethepicture/static/css/main.98208c79.chunk.css"
+  },
+  {
+    "revision": "1b1fa554dfeccd972e89",
+    "url": "/showmethepicture/static/js/2.e55e6146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showmethepicture/static/js/2.e55e6146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04818f02d04c1dd67b6",
+    "url": "/showmethepicture/static/js/main.02f883c2.chunk.js"
+  },
+  {
+    "revision": "4150d1fa61ce7516bc8e",
+    "url": "/showmethepicture/static/js/runtime-main.a7ef9b4c.js"
+  }
+]);
